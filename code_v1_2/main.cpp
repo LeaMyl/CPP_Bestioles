0233038594aca7fc78d6ac2bdd33eee109ff2c29
leami <leami@VICTUSLeaM.>
2025-03-20 08:25:20 +0100

    trying to fix

--- code_v1_2/main.cpp
+++ code_v1_2/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Milieu.h"
 #include "Bestiole.h"
 #include "ComportementGregaire.h"
@@ -49,57 +48,5 @@ int main() {
     // Démarre l'écosystème
     ecosysteme.run();
 
-=======
-#include <iostream>
-#include "Aquarium.h"
-#include "Milieu.h"
-#include "Bestiole.h"
-#include "BestioleFactory.h"
-#include "Configuration.h"
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
-
-const int WIDTH = 800;
-const int HEIGHT = 600;
-const int INIT_NBBESTIOLES = 10;
-const int FRAMERATE_DELAY = 30;
-
-int main()
-{
-    // Initialisation du générateur de nombres aléatoires
-    srand(time(NULL));
-    
-    // Chargement de la configuration
-    Configuration* config = Configuration::getInstance();
-    config->setConfigDefault();
-    
-    // Création de l'aquarium
-    Aquarium aquarium(WIDTH, HEIGHT, FRAMERATE_DELAY);
-    
-    // Récupération du milieu
-    Milieu& milieu = aquarium.getMilieu();
-    
-    // Création de la factory de bestioles
-    BestioleFactory factory;
-    
-    // Création des bestioles initiales
-    for (int i = 0; i < INIT_NBBESTIOLES; ++i) {
-        // Création d'une bestiole via la factory
-        IBestiole* bestiole = factory.IBestiole();
-        
-        // Initialisation des coordonnées aléatoires
-        bestiole->initCoords(WIDTH, HEIGHT);
-        
-        // Ajout de la bestiole au milieu
-        milieu.addMember(bestiole);
-    }
-    
-    // Lancement de la simulation
-    cout << "Début de la simulation..." << endl;
-    aquarium.run();
-    
->>>>>>> 1e9c5d9 (main version 1)
     return 0;
 }
