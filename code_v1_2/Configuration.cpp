a4d4680d77863f0a90f0da38bd4994c3b3a65288
Toxixce <titouan.dupasquier92@gmail.com>
2025-03-21 17:26:35 +0100

    Modification de la configuration

--- code_v1_2/Configuration.cpp
+++ code_v1_2/Configuration.cpp
@@ -1,95 +1,38 @@
-// Configuration.cpp
 #include "Configuration.h"
-#include <fstream>
-#include <iostream>
-#include <cmath>
 
+double Configuration::TAUX_GREGAIRE = 1/6;
+double Configuration::TAUX_PEUREUSE = 1/6;
+double Configuration::TAUX_KAMIKAZE = 1/6;
+double Configuration::TAUX_PREVOYANTE = 1/6;
+double Configuration::TAUX_MULTIPLE = 1/6;
+double Configuration::TAUX_NORMALE = 1/6;
 
-Configuration* Configuration::instance = nullptr;
+double Configuration::AFF_SIZE = 10;
+double Configuration::MAX_VITESSE = 5;
 
-Configuration::Configuration()
-{
-    setConfigDefault();
-}
+double Configuration::MAX_DELTA_O = 10;
+double Configuration::MIN_DELTA_O = 5;
+double Configuration::MAX_GAMMA_O = 10;
+double Configuration::MIN_GAMMA_O = 5;
+double Configuration::MAX_DELTA_Y = 10;
+double Configuration::MIN_DELTA_Y = 5;
+double Configuration::MAX_GAMMA_Y = 10;
+double Configuration::MIN_GAMMA_Y = 5;
+double Configuration::MAX_ALPHA = 10;
+double Configuration::MIN_ALPHA = 5;
 
-Configuration* Configuration::getInstance()
-{
-    if (instance == nullptr)
-        instance = new Configuration();
-    return instance;
-}
+double Configuration::MAX_NU = 10;
+double Configuration::MAX_OMEGA = 10;
+double Configuration::MAX_ETA = 10;
+double Configuration::MAX_PHI = 10;
+double Configuration::MIN_PHI = 5;
 
-void Configuration::setConfigDefault()
-{
-    // Comportements
-    tauxGregaire = 0.2;
-    tauxPeureuse = 0.2;
-    tauxKamikaze = 0.2;
-    tauxPrevoyante = 0.2;
-    tauxMultiple = 0.2;
-    
-    // Événements
-    tauxNaissanceSpontanee = 0.01;
-    probaMortCollision = 0.3;
-    probaClonage = 0.005;
-    
-    // Limites
-    vMax = 10.0;
-    rMax = 1.0;
-    etaMax = 1.0;
-    dMin = 0.0;
-    dMax = 30.0;
-    angleMin = 0.0;
-    angleMax = M_PI/2.0;
-    capaMin = 0.5;
-    capaMax = 1.0;
-    psiMin = 0.0;
-    psiMax = 1.0;
-    
-    // Caractéristiques
-    ageLimiteMin = 500;
-    ageLimiteMax = 2000;
-    tauxAccessoires = 0.7;
-    tauxCapteurs = 0.7;
-}
+double Configuration::GENERATION_RATE = 0.1;
+double Configuration::CLONE_RATE = 0.1;
+double Configuration::SURVIVE_COLLISION = 0.1;
 
-bool Configuration::loadFromFile(const std::string& filename)
-{
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cout << "Impossible d'ouvrir le fichier de configuration : " << filename << std::endl;
-        return false;
-    }
-    
-    std::string paramName;
-    double paramValue;
-    
-    while (file >> paramName >> paramValue) {
-        if (paramName == "tauxGregaire") tauxGregaire = paramValue;
-        else if (paramName == "tauxPeureuse") tauxPeureuse = paramValue;
-        else if (paramName == "tauxKamikaze") tauxKamikaze = paramValue;
-        else if (paramName == "tauxPrevoyante") tauxPrevoyante = paramValue;
-        else if (paramName == "tauxMultiple") tauxMultiple = paramValue;
-        else if (paramName == "tauxNaissanceSpontanee") tauxNaissanceSpontanee = paramValue;
-        else if (paramName == "probaMortCollision") probaMortCollision = paramValue;
-        else if (paramName == "probaClonage") probaClonage = paramValue;
-        else if (paramName == "vMax") vMax = paramValue;
-        else if (paramName == "rMax") rMax = paramValue;
-        else if (paramName == "etaMax") etaMax = paramValue;
-        else if (paramName == "dMin") dMin = paramValue;
-        else if (paramName == "dMax") dMax = paramValue;
-        else if (paramName == "angleMin") angleMin = paramValue;
-        else if (paramName == "angleMax") angleMax = paramValue;
-        else if (paramName == "capaMin") capaMin = paramValue;
-        else if (paramName == "capaMax") capaMax = paramValue;
-        else if (paramName == "psiMin") psiMin = paramValue;
-        else if (paramName == "psiMax") psiMax = paramValue;
-        else if (paramName == "ageLimiteMin") ageLimiteMin = static_cast<int>(paramValue);
-        else if (paramName == "ageLimiteMax") ageLimiteMax = static_cast<int>(paramValue);
-        else if (paramName == "tauxAccessoires") tauxAccessoires = paramValue;
-        else if (paramName == "tauxCapteurs") tauxCapteurs = paramValue;
-    }
-    
-    file.close();
-    return true;
-}
+// Constructeur
+Configuration::Configuration() {}
+
+// Destructeur
+Configuration::~Configuration() {}
